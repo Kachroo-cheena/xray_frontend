999027d3346e627c12031eee898e69944954fe5c
Cheena Kachroo <cheenakachroo2000@gmail.com>
2024-09-22 14:09:16 +0530

    backend url

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     formData.append('image', file);
 
     try {
-      const response = await axios.post('http://localhost:5000/predict', formData, {
+      const response = await axios.post('https://xray-backend-196i.onrender.com/predict', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
